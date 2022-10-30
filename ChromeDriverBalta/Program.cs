ac7aeae2c3d548733ebf59e57cbde1bdb7f0d025
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-10-30 13:40:24 -0300

    Add files via upload

---
+++ ChromeDriverBalta/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+using System;
+
+namespace ChromeDriverBalta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "https://youtube.com";
+
+            ChromeDriver cd = new ChromeDriver();
+            cd.Manage().Window.Maximize();
+            cd.Navigate().GoToUrl(url);
+
+            Thread.Sleep(1000); 
+
+            cd.FindElement(By.Name("search_query")).SendKeys("balta.io");
+            cd.FindElement(By.Id("search-icon-legacy")).Click();
+        }
+    }
+}
